21defe445b55b14e9fd9b4aa6173608e07a39d9b
musthafa <musthafa.cp@kalvium.community>
2023-12-12 21:46:41 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/thinkwithreactpart3/",
+  base:"/thinkWithReact_part3-2/",
   plugins: [react()],
   server : {
     port : 3000,
